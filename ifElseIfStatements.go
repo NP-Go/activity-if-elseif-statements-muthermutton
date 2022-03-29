bf2c391defd6ceaa58d82d6350eeb2ac2e2b4e3e
muthermutton <khairal.fazil@gmail.com>
2022-03-29 14:03:02 +0800

    added solution to A

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 	//Insert your code here
 	//Hint if a:= ?? ; condition {  }
+	if number := 17; number%2 == 0 {
+		resultMessage := strconv.Itoa(number) + " is even"
+		fmt.Println(resultMessage)
+	} else if number%2 == 1 {
+		resultMessage := strconv.Itoa(number) + " is odd"
+		fmt.Println(resultMessage)
+	}
 }
