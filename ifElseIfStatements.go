b464fd114b55ea63dfa113846a2387607a259a5c
khaifazil <khairal.fazil@gmail.com>
2022-03-31 19:28:56 +0800

    added check for number of digits and error catch

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -11,8 +11,20 @@ func main() {
 	if number := 17; number%2 == 0 {
 		resultMessage := strconv.Itoa(number) + " is even"
 		fmt.Println(resultMessage)
+		if number < 9 && number > 0 {
+			fmt.Println("The number has only 1 digit")
+		} else {
+			fmt.Println("The number has more than 1 digit")
+		}
 	} else if number%2 == 1 {
 		resultMessage := strconv.Itoa(number) + " is odd"
 		fmt.Println(resultMessage)
+		if number < 9 && number > 0 {
+			fmt.Println("The number has only 1 digit")
+		} else {
+			fmt.Println("The number has more than 1 digit")
+		}
+	} else {
+		fmt.Println("This is not a number")
 	}
 }
